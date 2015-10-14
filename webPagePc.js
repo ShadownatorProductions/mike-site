fd0b65138f58b33e0d7a1668cb3ff83ea97789d3
Chad Palmer <chadpalmer@gmail.com>
2015-10-14 16:13:36 -0400

    Fixed issues preventing original intended functionality from working.

--- webPagePc.js
+++ webPagePc.js
@@ -9,20 +9,20 @@ function init() {
 }
 
 function onCpu() {
-	getCpu = document.getElementById("cpu").value;
+	var getCpu = document.getElementById("cpu").value;
 	document.getElementById("gpu").style.display = "block";
 
-	if (getCpu == "Intel Pentium G3258") {
-		document.getElementById("gpu3").style.display = "none";
+	if (getCpu == "cpu1") {
+        document.getElementById("gpu3").style.display = "none";
 		document.getElementById("gpu4").style.display = "none";
 		document.getElementById("gpu5").style.display = "none";
-	} else if (getCpu == "Intel I3-4330") {
+	} else if (getCpu == "cpu2") {
 
     }
 }
 
 function onGpu() {
-	getGpu = document.getElementById("cpu").value;
+	var getGpu = document.getElementById("gpu").value;
 	document.getElementById("psu").style.display = "block";
 
 	if (getGpu == "") {
