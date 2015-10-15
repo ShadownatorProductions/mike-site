fc6b9a3ac73443fc5c25aac4f30b3a11a376d113
Chad Palmer <chadpalmer@gmail.com>
2015-10-15 16:18:50 -0400

    Added my attempt at a data file that will evetually ender the menus dynamically.

---
+++ data.js
@@ -0,0 +1,21 @@
+var data = {
+    "processors": [
+        {
+            "name": "Intel Pentium G3258",
+            "graphics": ["Nvidia GTX 750 TI", "Nvidia GTX 950"]
+        },
+        {
+            "name": "Intel I3-4330",
+            "graphics": ["Nvidia GTX 750 TI", "Nvidia GTX 950"]
+        },
+        {
+            "name": "Intel I5-4590",
+            "graphics": ["Nvidia GTX 750 TI", "Nvidia GTX 950"]
+        },
+        {
+            "name": "Intel Xeon E3-1241 V3",
+            "graphics": ["Nvidia GTX 750 TI", "Nvidia GTX 950"]
+        }
+    ],
+    "power": ["300W 80+ Bronze:", "500W 80+ Bronze:", "600W 80+ Bronze:"]
+};
