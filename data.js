9f86bf8cccba9c51c0c756d2271be2cc34a6dfa1
Chad Palmer <chadpalmer@gmail.com>
2015-10-22 17:12:30 -0400

    First stab at data file based on main.js file

--- data.js
+++ data.js
@@ -2,20 +2,167 @@ var data = {
     "processors": [
         {
             "name": "Intel Pentium G3258",
-            "graphics": ["Nvidia GTX 750 TI", "Nvidia GTX 950"]
+            "graphics": [
+                {
+                    "name": "Nvidia GTX 750 TI 2gb",
+                    "power": [
+                        {
+                            "name": "300 Watt 80+ Bronze",
+                            "ram": "I recommend 4 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "Nvidia GTX 950 2gb",
+                    "power": [
+                        {
+                            "name": "500 Watt 80+ Bronze",
+                            "ram": "I recommend 6 GB or more."
+                        }
+                    ]
+                }
+            ]
         },
         {
             "name": "Intel I3-4330",
-            "graphics": ["Nvidia GTX 750 TI", "Nvidia GTX 950"]
+            "graphics": [
+                {
+                    "name": "Nvidia GTX 750 TI 2gb",
+                    "power": [
+                        {
+                            "name": "300 Watt 80+ Bronze",
+                            "ram": "I recommend 4 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "Nvidia GTX 950 2gb",
+                    "power": [
+                        {
+                            "name": "500 Watt 80+ Bronze",
+                            "ram": "I recommend 6 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "AMD R9-380 4gb",
+                    "power": [
+                        {
+                            "name": "500 Watt 80+ Bronze",
+                            "ram": "I recommend 6 GB or more."
+                        }
+                    ]
+                }
+            ]
         },
         {
             "name": "Intel I5-4590",
-            "graphics": ["Nvidia GTX 750 TI", "Nvidia GTX 950"]
+            "graphics": [
+                {
+                    "name": "Nvidia GTX 750 TI 2gb",
+                    "power": [
+                        {
+                            "name": "300 Watt 80+ Bronze",
+                            "ram": "I recommend 4 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "Nvidia GTX 950 2gb",
+                    "power": [
+                        {
+                            "name": "500 Watt 80+ Bronze",
+                            "ram": "I recommend 6 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "AMD R9-380 4gb",
+                    "power": [
+                        {
+                            "name": "500 Watt 80+ Bronze",
+                            "ram": "I recommend 6 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "AMD R9-390 8gb",
+                    "power": [
+                        {
+                            "name": "600 Watt 80+ Bronze",
+                            "ram": "I recommend 8 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "AMD R9-390X 8gb",
+                    "power": [
+                        {
+                            "name": "700 Watt 80+ Bronze",
+                            "ram": "I recommend 12 GB or more."
+                        }
+                    ]
+                }
+            ]
         },
         {
             "name": "Intel Xeon E3-1241 V3",
-            "graphics": ["Nvidia GTX 750 TI", "Nvidia GTX 950"]
+            "graphics": [
+                {
+                    "name": "Nvidia GTX 750 TI 2gb",
+                    "power": [
+                        {
+                            "name": "300 Watt 80+ Bronze",
+                            "ram": "I recommend 4 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "Nvidia GTX 950 2gb",
+                    "power": [
+                        {
+                            "name": "500 Watt 80+ Bronze",
+                            "ram": "I recommend 6 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "AMD R9-380 4gb",
+                    "power": [
+                        {
+                            "name": "500 Watt 80+ Bronze",
+                            "ram": "I recommend 6 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "AMD R9-390 8gb",
+                    "power": [
+                        {
+                            "name": "600 Watt 80+ Bronze",
+                            "ram": "I recommend 8 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "AMD R9-390X 8gb",
+                    "power": [
+                        {
+                            "name": "700 Watt 80+ Bronze",
+                            "ram": "I recommend 12 GB or more."
+                        }
+                    ]
+                },
+                {
+                    "name": "Nvidia GTX 980 TI",
+                    "power": [
+                        {
+                            "name": "700 Watt 80+ Bronze",
+                            "ram": "I recommend 12 GB or more."
+                        }
+                    ]
+                }
+            ]
         }
-    ],
-    "power": ["300W 80+ Bronze:", "500W 80+ Bronze:", "600W 80+ Bronze:"]
+    ]
 };
