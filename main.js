b5d3686340c9cc671019a9536c3c9b553168cd17
Chad Palmer <chadpalmer@gmail.com>
2015-12-10 23:10:37 -0500

    All menus now work and extra code deleted

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ objMike.addEvents = function () {
 };
 
 objMike.renderCPU = function() {
+	var i;
 	var strHTML = '<option value="label">Choose a CPU</option>';
 	for (i = 0; i < data.processors.length; i++) {
 		strHTML += '<option value="' + data.processors[i].name + '">' + data.processors[i].name + '</option>';
@@ -28,6 +29,7 @@ objMike.renderCPU = function() {
 };
 
 objMike.renderGPU = function(numCPU) {
+	var i;
 	var strHTML = '<option value="label">Choose a GPU</option>';
 	if (numCPU !== 0) {
 		for (i = 0; i < data.processors[numCPU - 1].graphics.length; i++) {
@@ -35,6 +37,24 @@ objMike.renderGPU = function(numCPU) {
 		}
 		objMike.arrMenus.eq(1).html(strHTML);
 		objMike.arrMenus.eq(1).addClass("active");
+		objMike.numCPUSelect = numCPU;
+	}
+};
+
+objMike.renderPowRam = function(numGPU) {
+	var i;
+	var strHTMLPower = '<option value="label">Choose a power supply</option>';
+	var strHTMLRam = '<option value="label">Choose RAM</option>';
+	if (numGPU !== 0) {
+		for (i = 0; i < data.processors[objMike.numCPUSelect - 1].graphics[numGPU - 1].power.length; i++) {
+			strHTMLPower += '<option value="' + data.processors[objMike.numCPUSelect - 1].graphics[numGPU - 1].power[i] + '">' + data.processors[objMike.numCPUSelect - 1].graphics[numGPU - 1].power[i] + '</option>';
+		}
+		for (i = 0; i < data.processors[objMike.numCPUSelect - 1].graphics[numGPU - 1].ram.length; i++) {
+			strHTMLRam += '<option value="' + data.processors[objMike.numCPUSelect - 1].graphics[numGPU - 1].ram[i] + '">' + data.processors[objMike.numCPUSelect - 1].graphics[numGPU - 1].ram[i] + '</option>';
+		}
+		objMike.arrMenus.eq(2).html(strHTMLPower);
+		objMike.arrMenus.eq(3).html(strHTMLRam);
+		objMike.arrMenus.slice(2).addClass("active");
 	}
 };
 
@@ -44,67 +64,6 @@ objMike.changeCPU = function() {
 };
 
 objMike.changeGPU = function() {
-	console.log(objMike.arrMenus.eq(1)[0].selectedIndex);
+	objMike.arrMenus.slice(2).removeClass("active");
+	objMike.renderPowRam(objMike.arrMenus.eq(1)[0].selectedIndex);
 };
-
-function onCpu() {
-	getCpu = document.getElementById("cpu").value;
-	document.getElementById("cpuLabel").style.display = "none";
-	document.getElementById("gpu").style.display = "block";
-	
-	if (getCpu == "Pentium G3258") {
-		document.getElementById("gpu3").style.display = "none";	
-		document.getElementById("gpu4").style.display = "none";		
-		document.getElementById("gpu5").style.display = "none";		
-		document.getElementById("gpu6").style.display = "none";		
-	} else if (getCpu == "I3-4330") {
-		document.getElementById("gpu4").style.display = "none";	
-		document.getElementById("gpu5").style.display = "none";		
-		document.getElementById("gpu6").style.display = "none";		
-		document.getElementById("gpu3").style.display = "block";
-	} else if (getCpu == "I5-4590") {
-		document.getElementById("gpu6").style.display = "none";
-		document.getElementById("gpu4").style.display = "block";
-		document.getElementById("gpu5").style.display = "block";
-	} else {
-		document.getElementById("gpu1").style.display = "block";
-		document.getElementById("gpu2").style.display = "block";
-		document.getElementById("gpu3").style.display = "block";
-		document.getElementById("gpu4").style.display = "block";
-		document.getElementById("gpu5").style.display = "block";
-		document.getElementById("gpu6").style.display = "block";
-	}
-}
-
-function onGpu() {
-	getGpu = document.getElementById("gpu").value;
-	document.getElementById("gpuLabel").style.display = "none";
-	document.getElementById("psu").style.display = "block";
-	
-	if (getGpu == "Nvidia GTX 750 TI 2gb") {
-		document.getElementById("psu2").style.display = "none";
-		document.getElementById("psu3").style.display = "none";
-		document.getElementById("psu4").style.display = "none";
-	} else if (getGpu == "Nvidia GTX 950 2gb" || getGpu == "AMD R9-380 4gb") {
-		document.getElementById("psu1").style.display = "none";
-		document.getElementById("psu2").style.display = "block";
-		document.getElementById("psu3").style.display = "none";
-		document.getElementById("psu4").style.display = "none";
-	} else if (getGpu == "AMD R9-390 8gb") {
-		document.getElementById("psu1").style.display = "none";
-		document.getElementById("psu2").style.display = "none";
-		document.getElementById("psu3").style.display = "block";
-		document.getElementById("psu4").style.display = "none";
-	}
-}
-
-function onPsu() {
-	getPsu = document.getElementById("psu").value;
-	document.getElementById("psuLabel").style.display = "none";
-	document.getElementById("ram").style.display = "block";
-	
-}
-
-function onRam() {
-	
-}
